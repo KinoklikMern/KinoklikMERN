63670727bfe8a5fa5fd47584abbe98a7516e9109
Costin Dumitrescu <53879882+costindumitrescu@users.noreply.github.com>
2022-11-12 17:30:40 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import movieRoutes from './routes/movies.js';
+
+const app = express();
+
+app.use('/movies', movieRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+app.listen(8000, () =>
+  console.log(`App Running on PORT ${PORT}`),
+);
+
+const CONNECTION_URL = 'mongodb+srv://kinoklik:KinoKlik99!!@kinoklik.sk2izbk.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 8000;
+
+mongoose.connect(CONNECTION_URL, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+}, err => {
+   if(err) throw err;
+   console.log(`Connected to MongoDB on PORT: ${PORT}!!!`)
+})
