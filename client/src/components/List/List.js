6339c708734941802ba0c4dc25a59f0be6834b1e
wuzhe66 <91699733+wuzhe66@users.noreply.github.com>
2022-11-21 20:06:02 -0500

    Show loop posters

--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -44,16 +44,7 @@ export default function List() {
                     style={{ display: !isMoved && "none" }}
                 />
                 <div className="container" ref={listRef}>
-                    <ListItem index={0} />
-                    <ListItem index={1} />
-                    <ListItem index={2} />
-                    <ListItem index={3} />
-                    <ListItem index={4} />
-                    <ListItem index={5} />
-                    <ListItem index={6} />
-                    <ListItem index={7} />
-                    <ListItem index={8} />
-                    <ListItem index={9} />
+                    <ListItem />
                 </div>
                 <ArrowForwardIosOutlined
                     className="sliderArrow right"
--- client/src/components/ListItem/ListItem.js
+++ client/src/components/ListItem/ListItem.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import "./ListItem.css";
-import {
-    PlayArrow,
-    Add,
-    ThumbUpAltOutlined,
-    ThumbDownOutlined,
-} from "@material-ui/icons";
-import { useState } from "react";
-
-export default function ListItem({ index }) {
-
-    const [isHovered, setIsHovered] = useState(false);
+import IMAGES from "../../images/index.js";
+export default function ListItem({ }) {
 
     return (
-        <div className="listItem">
-            <img
-                src="https://m.media-amazon.com/images/I/71PR1evv9aL._AC_SY879_.jpg"
-                alt=""
-            />
+        <>
+            {
+                IMAGES && IMAGES.map((item) =>
+                    <div className="listItem" key={item.id}>
+                        <img src={item.image} alt="" />
+                    </div>
+                )
 
-        </div>
+            }
+        </>
     );
 }
