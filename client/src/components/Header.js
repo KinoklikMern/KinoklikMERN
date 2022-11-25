7c73af22e3686c47ccd2a7d599062e828fdcf83a
JJredfield7 <JJredfield7@users.noreply.github.com>
2022-11-25 09:32:07 -0800

    Nav edit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -77,7 +77,7 @@ function Header() {
                 </span>
                 <span>
                   <Link className="whatsnew-text" to="/upload">
-                    &nbsp;&nbsp;&nbsp;&nbsp;Upload movie
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Upload FILM EPK
                   </Link>
                 </span>
               </button>
