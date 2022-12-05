ae7ea9a4bb4dd8010e56dcc5db4639190e462bad
wuzhe66 <91699733+wuzhe66@users.noreply.github.com>
2022-12-05 11:18:39 -0500

    front end

--- client/src/components/Epk/Present/director.js
+++ client/src/components/Epk/Present/director.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button, Col, Row } from "antd";
 
 import "./director.css";
 
@@ -6,16 +7,43 @@ function Director(directorFile) {
     console.log(directorFile);
 
     return (
-        <div className="container">
-            <img
-                src={directorFile.directorFile.image}
-                alt="hey"
-                style={{ height: "50%" }}
-            />
-            <div className="centered">
-                <h3>{directorFile.directorFile.text}</h3>
-            </div>
-        </div>
+
+
+        <>
+
+            {/* <div className="container">
+                <img
+                    src={directorFile.directorFile.image}
+                    alt="hey"
+                    style={{ height: "50%" }}
+                />
+                <div className="centered">
+                    <h3>{directorFile.directorFile.text}</h3>
+                </div>
+            </div> */}
+
+
+            {" "}
+            <br />
+            <Row
+                justify="space-around"
+                className="text-center "
+            >
+                <Col span={6} className="m-2 bg-light">
+                    <h4>Director</h4>
+                    <h3>{directorFile.directorFile.text}</h3>
+
+                    <img
+                        src={directorFile.directorFile.image}
+                        alt="hey"
+                        style={{ height: "50%" }}
+                    />
+                </Col>
+            </Row>
+
+        </>
+
+
     );
 }
 export default Director;
