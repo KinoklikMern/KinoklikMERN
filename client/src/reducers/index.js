0fd94d056d59209a8694b29be696653911053138
Marcelo Di Liscia <114948869+MarceloKlik@users.noreply.github.com>
2022-12-12 11:05:06 -0500

    Merged with Marcelo-Epk
    Merged with Marcelo-Epk

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,9 +3,5 @@ import { combineReducers } from 'redux';
 import movies from "./movies";
 import { userReducer } from "./userReducer";
 
-import { combineReducers } from "redux";
-
-import { userReducer } from "./userReducer";
-
 export default combineReducers({ movies: movies, user: userReducer });
 
