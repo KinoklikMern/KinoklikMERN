974aeb996ea8986d64c9eb99ebd1e70a311f335f
Marcelo Di Liscia <114948869+MarceloKlik@users.noreply.github.com>
2022-12-12 12:23:14 -0500

    Revert "Merged with Marcelo-Epk"
    This reverts commit 0fd94d056d59209a8694b29be696653911053138.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,5 +3,9 @@ import { combineReducers } from 'redux';
 import movies from "./movies";
 import { userReducer } from "./userReducer";
 
+import { combineReducers } from "redux";
+
+import { userReducer } from "./userReducer";
+
 export default combineReducers({ movies: movies, user: userReducer });
 
