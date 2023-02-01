f6b677d1b9ba4070979e693d5dbabdf2981522de
Kyle Piché <kylepiche@gmail.com>
2023-02-01 12:35:45 -0500

    Initial

--- server/index.js
+++ server/index.js
@@ -11,11 +11,13 @@ import epkRoutes from "./routes/epk.js";
 import fepkRoutes from "./routes/fepk.js";
 import crewRoutes from "./routes/crew.js";
 
+import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/movies", movieRoutes);
 app.use("/users", userRoutes);
