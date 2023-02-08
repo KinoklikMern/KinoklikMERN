9cdc687559c88c6ac2a974273a08b8d3539ec5da
zeenw <111456142+zeenw@users.noreply.github.com>
2023-02-08 09:16:59 -0500

    change route to page

--- client/src/pages/FlimMaker/filmMakerDashboard.js
+++ client/src/pages/FlimMaker/filmMakerDashboard.js
@@ -1,11 +1,18 @@
+////////////////////////////////////////////////
+// Create filmMakerDashboard page
+// Edit by Tony 
+// On Jan 15, 2023
+////////////////////////////////////////////////
 import { useSelector } from "react-redux";
-import React from 'react'
+import { React, useEffect, useState } from 'react'
+import Axios from "axios"
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { faComments} from '@fortawesome/free-solid-svg-icons'
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { faHome} from '@fortawesome/free-solid-svg-icons'
 import { faInbox } from '@fortawesome/free-solid-svg-icons'
 import { faBell} from '@fortawesome/free-solid-svg-icons'
@@ -23,162 +30,128 @@ import { faPeopleGroup } from '@fortawesome/free-solid-svg-icons'
 import { faCrown } from '@fortawesome/free-solid-svg-icons'
 import { Link, NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './filmMakerDashboard.scss'
+//import './filmMakerDashboard.scss'
 import movie6 from '../../images/movies/movie6.jpg';
 import movie2 from '../../images/movies/movie2.jpeg';
 import movie5 from '../../images/movies/movie5.jpg';
 
 export default function Filmmaker() {
+  const [epkList, setEpkList] = useState([]);
+  useEffect(() => {
+    try {
+      Axios.get(process.env.REACT_APP_BACKEND_URL + "/filmMakerDashboard").then((rs) => {
+        setEpkList(rs.data);
+      });
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  }, []);
+
   return (
-    
-<div class="filmmakerdash-container container-fluid">
-  <div class="sidebar-container"> 
-    <div class="sidebar-left">
-      <div class="filmmaker-navbar px-1">   
-        <ul class="nav nav-tabs filmmaker-dash-ul" role="tablist">
+    <div class="filmmakerdash-container container-fluid">
+      <div class="sidebar-container"> 
+        <div class="sidebar-left">
+          
+          <ul class="nav nav-tabs filmmaker-dash-ul">
 
-          <li class="nav-link" role="tab" data-li="UploadMovie">
-            <div class="sidebarnav-icon side-button">
-            <a href="/Notification.js">
-            <FontAwesomeIcon icon={faNewspaper} />  
-            </a>
-            </div>
-          </li>
-          <li class="nav-item" role="presentation">
-            <div class="nav-link tab-clickable" data-bs-toggle="tab" data-bs-target="#dashboard" role="tab" >
-            <div class="sidebarnav-icon side-button">
-            <FontAwesomeIcon icon={faPhotoFilm} />  
-            </div>  
-            </div>
-          </li>
-
-          <li class="nav-item" role="presentation">
-            <div class="nav-link tab-clickable" data-bs-toggle="tab" data-bs-target="#inbox" role="tab" >
-            <div class="sidebarnav-icon side-button">
-            <Link to="/filmMakerDashboard" class="links"><FontAwesomeIcon icon={faHome}/></Link>   
-            </div>
-            </div>
-          </li>
-
-          <li class="nav-item" role="presentation">
-            <div class="nav-link tab-clickable" data-bs-toggle="tab" data-bs-target="#notifications" role="tab" >
-            <div class="sidebarnav-icon side-button">
-            <FontAwesomeIcon icon={faBell} />   
-            </div>
-             </div>
-          </li>
-
-          <li class="nav-item" role="presentation">
-             <div class="nav-link tab-clickable" data-bs-toggle="tab" data-bs-target="#filmList" role="tab" >
-             <div class="sidebarnav-icon side-button">
-             <br/>
-             </div>
-              </div>
-          </li>
-
-          <li class="nav-item" role="presentation">
-              <div class="nav-link tab-clickable" data-bs-toggle="tab" data-bs-target="#settings" role="tab" >
+            <li>
               <div class="sidebarnav-icon side-button">
-              <br/>    
+                <a href="/Notification.js">
+                  <FontAwesomeIcon icon={faNewspaper} />  
+                </a>
               </div>
-              </div>
-          </li>
+            </li>
 
-          <li class="nav-link" role="tab" data-li="UserProfile">
-          <div class="sidebarnav-icon side-button">
-            
-            <Link to="/filmMakerDashboardSecurity" class="links"><FontAwesomeIcon icon={faCog}/></Link>
-            
+            <li class="nav-item" role="presentation">
+              <div class="nav-link tab-clickable" data-bs-toggle="tab" data-bs-target="#dashboard" role="tab" >
+                <div class="sidebarnav-icon side-button">
+                  <FontAwesomeIcon icon={faPhotoFilm} />  
+                </div>  
+              </div>
+            </li>
+
+            <li class="nav-item" role="presentation">
+              <div class="nav-link tab-clickable" data-bs-toggle="tab" data-bs-target="#inbox" role="tab" >
+                <div class="sidebarnav-icon side-button">
+                  <Link to="/filmMakerDashboard" class="links"><FontAwesomeIcon icon={faHome}/></Link>   
+                </div>
+              </div>
+            </li>
+
+            <li class="nav-item" role="presentation">
+              <div class="nav-link tab-clickable" data-bs-toggle="tab" data-bs-target="#notifications" role="tab" >
+                <div class="sidebarnav-icon side-button">
+                  <FontAwesomeIcon icon={faBell} />   
+                </div>
+              </div>
+            </li>
+
+
+
+
+
+            <li class="nav-link" role="tab" data-li="UserProfile">
+              <div class="sidebarnav-icon side-button">
+                <Link to="/filmMakerDashboardSecurity" class="links"><FontAwesomeIcon icon={faCog}/></Link>
+              </div>
+            </li>
+          </ul>
+          
+        </div>
+
+        <div class="sidebar-right">
+          <article class="tab-pane fade show active" role="tabpanel" aria-labelledby="llanfairpwllgwyngyll-left-tab" id="dashboard">
+            <div class=" sidebar-rightcontainer">
+              <div class="item Dashboard">
+                <h1>Filmmaker Dashboard</h1>
+                <h1> <Link to="/uploadEpk" class="icon-plus"> <FontAwesomeIcon icon={faPlus} /> </Link></h1>
+                
+                { (!epkList) ? ( () => (    
+                  <div>
+                    <p class="icon-plus">
+                      You don`t have any EPK created.To start promoting your film right away.
+                    </p>
+                    <Button> Create your free film EPK now! </Button>
+                  </div>
+                )) : null }
+
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                  {epkList.map((epk) => (
+                    <div class="col">
+                      <Link to="/filmMakerSelectedMovie" class="links">
+                        <div class="card movie-card">
+                        
+                          <img src={epkList.banner_url} alt="movie banner"/>
+                                
+                          <div class="card-body">
+                            
+                            <div class="d-flex justify-content-between align-items-center pb-1 small-numbers">
+                              <p>{(epk.wishes_to_buy == null) ? "0" : epk.wishes_to_buy.length}</p>
+                              <p>{(epk.favourites == null) ? "0" : epk.favourites.length}</p>
+                              <p>{(epk.likes == null) ? "0" : epk.likes.length}</p>
+                              <p>{(epk.sharings == null) ? "0" : epk.sharings.length}</p>
+                            </div>
+                            
+                            <div class="d-flex justify-content-between align-items-center pb-1">
+                              <FontAwesomeIcon icon={faDollarSign} />
+                              <FontAwesomeIcon icon={faStar} />  
+                              <FontAwesomeIcon icon={faBookmark} /> 
+                              <FontAwesomeIcon icon={faShareNodes} /> 
+                            </div>
+                          </div>
+                        </div>
+                      </Link>
+                    </div>
+                  ))}
+                </div>
+                <div class="side-id">
+                  <FontAwesomeIcon icon={faUser}/> 
+                </div>
+              </div>  
             </div>
-          </li>
-        </ul>
+          </article>
+        </div>
       </div>
     </div>
-
-    <div class="sidebar-right">
-    <article class="tab-pane fade show active" role="tabpanel" aria-labelledby="llanfairpwllgwyngyll-left-tab" id="dashboard">
-<div class=" sidebar-rightcontainer">
-    <div class="item Dashboard">
-    <h1>Filmmaker Dashboard</h1>
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-            
-            
-            <div class="col">
-            <Link to="/filmMakerSelectedMovie" class="links">
-              
-              <div class="card movie-card">
-              
-              <img src={movie6} alt="movie 6"/>         
-                <div class="card-body">
-                  
-                <div class="d-flex justify-content-between align-items-center pb-1 small-numbers">
-                  <p>200</p>
-                  <p>200</p>
-                  <p>200</p>
-                  <p>200</p>
-                  </div>
-                  
-                  <div class="d-flex justify-content-between align-items-center pb-1">
-                    
-                    
-                    <FontAwesomeIcon icon={faDollarSign} />
-                    <FontAwesomeIcon icon={faStar} />  
-                    <FontAwesomeIcon icon={faBookmark} /> 
-                    <FontAwesomeIcon icon={faShareNodes} /> 
-                    
-                    
-                  </div>
-                </div>
-              </div></Link>
-            </div>
-            
-            <div class="col">
-              <div class="card movie-card">
-              <img src={movie2} alt="movie 2"/>
-                <div class="card-body">
-                  
-
-
-                <div class="d-flex justify-content-between align-items-center pb-1 small-numbers">
-                  <p>200</p>
-                  <p>200</p>
-                  <p>200</p>
-                  <p>200</p>
-                </div>
-                  
-                  
-                <div class="d-flex justify-content-between align-items-center pb-1">
-                    
-                    <FontAwesomeIcon icon={faDollarSign} />
-                    <FontAwesomeIcon icon={faStar} />  
-                    <FontAwesomeIcon icon={faBookmark} /> 
-                    <FontAwesomeIcon icon={faShareNodes} /> 
-                    
-
-                  </div>
-                </div>
-              </div>
-            </div>
-            
-            
-
-            
-          </div>
-          <div class="side-id">
-            <FontAwesomeIcon icon={faUser}/> 
-            </div>
-          
-        </div>  
-        
-    </div>
-
-    
-</article>
-
-
-  
-    </div>
-  </div>
-</div>
   );
 }
