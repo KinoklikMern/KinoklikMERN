9cc5efa41ef366e9da08d08ba310bb5eb06c5249
Kyle Piché <kylepiche@gmail.com>
2023-02-15 00:55:10 -0500

    Fixed KinoKlik glowing blue on hover

--- client/src/components/navbar/NavbarBrand.js
+++ client/src/components/navbar/NavbarBrand.js
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import KinoKlikIcon from "../../images/logo.png";
 
 function NavbarBrand() {
   return (
     <div className="">
-      <a href="#" className="tw-flex tw-items-center tw-text-6xl tw-text-white">
+      <Link to="/" className="tw-flex tw-items-center tw-text-6xl tw-text-white hover:tw-text-white">
         <img src={KinoKlikIcon} alt="KinoKlik Logo" className=" tw-h-20"></img>
         KinoKlik
-      </a>
+      </Link>
     </div>
   );
 }
