7bfc5d8b74436f17f5f5d7ffcb3904438d27320e
Kyle Piché <kylepiche@gmail.com>
2023-02-23 12:53:53 -0500

    update user model to remove movie ref

--- server/models/User.js
+++ server/models/User.js
@@ -42,22 +42,6 @@ const UserSchema = mongoose.Schema({
       "Viewer",
     ],
   },
-  movies: [
-    {
-      movie: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "movie",
-      },
-    },
-  ],
-  uploaded_movies: [
-    {
-      movie: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "movie",
-      },
-    },
-  ],
   following: {
     type: Array,
     default: [],
