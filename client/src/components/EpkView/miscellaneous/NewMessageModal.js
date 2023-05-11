44624322c84261b2057e163834a4d8d2f4c0ec92
rucheng <rrcjadef@gmail.com>
2023-05-11 13:32:34 -0400

    add message icon and function for epk view page

---
+++ client/src/components/EpkView/miscellaneous/NewMessageModal.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { ChatState } from "../../../context/ChatProvider";
+import { addToChat } from "../../../api/epks";
+import { io } from "socket.io-client";
+
+let socket;
+export default function NewMessageModal(props) {
+  const { notification, setNotification } = ChatState();
+  const [socketConnected, setSocketConnected] = useState(false);
+  const [msg, setMsg] = useState("");
+  const handleChange = (e) => {
+    setMsg(e.target.value);
+  };
+  const handleSubmit = () => {
+    if (msg.length > 0) {
+      try {
+        addToChat(msg, props.user, props.filmmakerId).then((res) => {
+          if (res.status == 200) {
+            socket.emit("new message", res.data);
+            props.close("message");
+            props.setRefresh(true);
+          }
+        });
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    socket = io(process.env.REACT_APP_BACKEND_URL);
+    socket.emit("setup", props.user);
+    socket.on("connection", () => setSocketConnected(true));
+  }, []);
+
+  useEffect(() => {
+    // console.log("selectchat", selectedChatCompare);
+    socket.on("message recieved", (newMessageRecieved) => {
+      // notification
+      if (!notification.includes(newMessageRecieved)) {
+        setNotification([newMessageRecieved, ...notification]);
+      }
+    });
+  });
+  return (
+    <>
+      <Modal
+        show={() => props.open("message")}
+        onHide={() => props.close("message")}
+        centered
+        className="p-3"
+      >
+        <Modal.Header className="border-0">
+          <Modal.Title className="text-center px-3">
+            Please type your message to the Filmmaker EPK Owner
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group
+              className="my-3"
+              controlId="exampleForm.ControlTextarea1"
+              value={msg}
+              onChange={handleChange}
+            >
+              <Form.Control
+                style={{ height: "200px", resize: "none" }}
+                as="textarea"
+                rows={4}
+                placeholder="eg. Hello Filmmaker, I’m interested to see your film EPK and possibly purchase the rights. Let’s connect and talk! "
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer
+          style={{ border: "none", display: "flex", justifyContent: "center" }}
+        >
+          <Button
+            style={{
+              backgroundColor: "#fff",
+              border: "none",
+              color: "#1E0039",
+              boxShadow: "3px 3px 10px #712CB0",
+              width: "25%",
+              padding: "0",
+            }}
+            onClick={handleSubmit}
+          >
+            Send
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
