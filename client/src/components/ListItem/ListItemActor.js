839a4a31c7ff075b7ba665dfabe24f172a99c09f
Delmayara <delara.amiri86@gmail.com>
2023-10-26 09:19:45 -0400

    list actor style

--- client/src/components/ListItem/ListItemActor.js
+++ client/src/components/ListItem/ListItemActor.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./ListItemActor.css";
 import { useState, useRef, useEffect } from "react";
-import { Link } from "react-router-dom";
 import http from "../../http-common";
 import { useSelector } from "react-redux";
 
