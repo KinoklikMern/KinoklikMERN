fd602fbd856c66ecb31591abface1a3d3129f03d
Malek Kazal <ghazal.malek@gmail.com>
2023-11-14 12:36:25 -0500

    LandingPage Head

--- client/src/components/LandingPage/Landing1.js
+++ client/src/components/LandingPage/Landing1.js
@@ -40,16 +40,16 @@ const Landing1 = () => {
 
   return (
     <>
-      <div className='tw-h-[100vh] tw-bg-midnight'>
-        <div className='tw-p-24'>
-          <div className='tw-flex tw-w-full tw-items-center tw-overflow-hidden'>
-            <div className='md:tw-w-2/4'>
-              <h1 className='tw-mt-6 tw-text-start tw-text-7xl tw-font-bold tw-text-white'>
+      <div className='tw-bg-midnight md:tw-h-[100vh]'>
+        <div className='md:tw-p-24'>
+          <div className='tw-flex tw-w-full tw-items-center'>
+            <div className='tw-p-7 lg:tw-w-2/4'>
+              <h1 className='tw-mt-6 tw-text-start tw-text-6xl tw-font-bold tw-text-white md:tw-text-7xl'>
                 Film marketing.
                 <br />
                 Refined.
               </h1>
-              <p className='tw-my-12 tw-text-2xl tw-font-semibold tw-text-white'>
+              <p className='tw-my-12 tw-text-xl tw-font-semibold tw-text-white md:tw-text-2xl'>
                 Promote your film to industry professionals with KinoKlik's free
                 <br />
                 <span className='tw-text-[#FF00F5]'>
@@ -59,13 +59,13 @@ const Landing1 = () => {
               </p>
               <div className='tw-mt-4 tw-grid-cols-2 sm:tw-mt-6'>
                 <a
-                  className='tw-mr-4 tw-inline-block tw-rounded-lg tw-bg-[#FF00F5] tw-px-4 tw-py-2 tw-text-xl tw-font-bold  tw-tracking-wider tw-text-midnight tw-shadow-lg hover:tw--translate-y-0.5 hover:tw-bg-violet-600 focus:tw-outline-none sm:tw-text-base'
+                  className='tw-mr-4 tw-inline-block tw-rounded-lg tw-bg-[#FF00F5] tw-px-3 tw-py-1 tw-text-xl tw-font-bold tw-tracking-wider tw-text-midnight  tw-shadow-lg hover:tw--translate-y-0.5 hover:tw-bg-violet-600 focus:tw-outline-none sm:tw-text-base md:tw-px-4 md:tw-py-2'
                   href='/uploadFepk'
                 >
                   Create EPK
                 </a>
                 <a
-                  className='tw-inline-block tw-rounded-lg tw-bg-white tw-px-4 tw-py-2 tw-text-xl tw-font-bold tw-tracking-wider tw-text-[#1E0039] tw-shadow-lg hover:tw--translate-y-0.5 hover:tw-bg-violet-600 focus:tw-outline-none sm:tw-text-base'
+                  className='tw-inline-block tw-rounded-lg tw-bg-white tw-px-3 tw-py-1 tw-text-xl tw-font-bold tw-tracking-wider tw-text-[#1E0039] tw-shadow-lg hover:tw--translate-y-0.5 hover:tw-bg-violet-600 focus:tw-outline-none sm:tw-text-base md:tw-px-4 md:tw-py-2'
                   href='/catalog'
                 >
                   Browse EPKs
@@ -73,13 +73,13 @@ const Landing1 = () => {
               </div>
             </div>
 
-            <div className='tw-invisible tw-grid tw-w-2/4 tw-grid-cols-2 lg:tw-visible'>
+            <div className='tw-invisible tw-grid tw-w-0 md:tw-grid-cols-2 lg:tw-visible lg:tw-w-2/4'>
               {filteredFepks.map((fepk, index) => (
                 <img
                   key={index}
                   className={`${
                     index === 0 ? "tw-row-span-1" : "tw-row-span-2"
-                  } tw-rounded-lg tw-object-cover`}
+                  } tw-m-3 tw-scale-150 tw-rounded-lg tw-object-cover`}
                   src={`${process.env.REACT_APP_AWS_URL}/${fepk.image_details}`}
                   alt=''
                 />
