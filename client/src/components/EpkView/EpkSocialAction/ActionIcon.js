1585324d840e1d91980a43b4a3999d147a7c3b32
Malek Kazal <ghazal.malek@gmail.com>
2023-11-20 13:57:15 -0500

    LandingPage content - favicon - website title - Epk Page Action Icons hover

--- client/src/components/EpkView/EpkSocialAction/ActionIcon.js
+++ client/src/components/EpkView/EpkSocialAction/ActionIcon.js
@@ -1,19 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
-export default function ActionIcon({ name, icon, handlers, number }) {
+export default function ActionIcon({ name, icon, handlers, number, title }) {
   const { clickHandler, hoverHandler } = handlers;
   return (
     <div
-      className=" tw-relative tw-inline-flex tw-h-16 tw-w-16 tw-justify-center tw-bg-[#712CB0] hover:tw-scale-110"
+      className=' tw-relative tw-inline-flex tw-h-16 tw-w-16 tw-justify-center tw-bg-[#712CB0] hover:tw-scale-110'
       style={{ borderRadius: "20px", cursor: "pointer" }}
       onClick={() => clickHandler(name)}
+      title={title}
     >
       <img
         src={icon}
-        alt=""
+        alt=''
         style={{ height: "60%" }}
-        className="tw-overflow-visible"
+        className='tw-overflow-visible'
       />
       <span
         className={`tw-absolute tw--right-3 tw--top-1 tw-text-sm tw-font-bold tw-text-white`}
