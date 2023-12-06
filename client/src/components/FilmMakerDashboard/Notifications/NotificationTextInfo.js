206e96e4de68b2c68121236dbffb584a3cc3af8f
dinarayakupova <dinarayakupova@hotmail.com>
2023-12-06 10:46:46 -0500

    EPK section text + notification section text

---
+++ client/src/components/FilmMakerDashboard/Notifications/NotificationTextInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NotificationTextInfo() {
+    return (
+        <div className="tw-flex tw-flex-col tw-justify-start tw-items-start ">
+          <p className="tw-text-sm tw-font-bold tw-text-[#1E0039] tw-text-center">
+          Find all the users film industry professionals who favourite and follow your film, and those who requests access to your protected EPKs.
+          </p>
+        </div>
+      );
+    }
