99a69bc809de00fc34c4665cbdab3445faa7c83a
Malek Kazal <ghazal.malek@gmail.com>
2023-12-12 11:51:13 -0500

    Actor dashboard

--- client/src/components/UserDashboard/Upload/UploadActorPicCon.js
+++ client/src/components/UserDashboard/Upload/UploadActorPicCon.js
@@ -32,21 +32,10 @@ export default function UploadActorPicCon() {
 
   return (
     <div className='tw-flex tw-h-screen tw-flex-col tw-overflow-hidden tw-bg-[#1E0039]'>
-      <div
-        className='tw-mt-24 tw-flex tw-justify-start tw-pl-24 tw-text-white md:tw-mb-8'
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
+      <div className='tw-mt-24 tw-flex tw-flex-col tw-items-center tw-justify-center tw-text-white md:tw-mb-8 md:tw-flex-row md:tw-justify-between md:tw-pl-24'>
         <p className='tw-text-4xl'>{t("Actor Dashboard")}</p>
         <a href={`/actor/${userId}`}>
-          <p
-            className='tw-rounded-2xl tw-bg-black tw-p-2 md:tw-text-3xl'
-            style={{
-              marginRight: "20px",
-            }}
-          >
+          <p className='tw-mt-5 tw-rounded-2xl tw-bg-white tw-p-2 tw-font-semibold tw-text-midnight md:tw-mr-24 md:tw-mt-0 md:tw-text-3xl'>
             {t("Actor Page")}
           </p>
         </a>
@@ -55,14 +44,8 @@ export default function UploadActorPicCon() {
         <div className='tw-mt-12 tw-h-5/6 md:tw-ml-16'>
           <Sidebar selectedTab='Actor Page' role={user.role} />
         </div>
-        <div className='tw-scrollbar-w-36 tw-mt-12 tw-h-5/6 tw-overflow-auto tw-rounded-lg tw-bg-white tw-p-4 tw-scrollbar tw-scrollbar-track-gray-500 tw-scrollbar-thumb-[#1E0039]  md:tw-ml-16 md:tw-w-5/6'>
-          <div
-            className='tw-flex tw-flex-col tw-gap-12'
-            style={{
-              display: "grid",
-              justifyItems: "center",
-            }}
-          >
+        <div className='tw-scrollbar-w-36 tw-mt-4 tw-h-5/6 tw-overflow-auto tw-rounded-lg tw-bg-white tw-p-4 tw-scrollbar tw-scrollbar-track-gray-500 tw-scrollbar-thumb-[#1E0039] md:tw-ml-16  md:tw-mt-12 md:tw-w-5/6'>
+          <div className='tw-flex tw-flex-col tw-items-center tw-gap-12'>
             {loading ? (
               <LoadingSpin />
             ) : epkList.length === 0 ? (
@@ -75,7 +58,7 @@ export default function UploadActorPicCon() {
                 <UploadActorPic user={user} />
                 <div>
                   <List />
-                </div>                
+                </div>
               </>
             )}
           </div>
