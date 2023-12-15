f8f13b28038b52b9e64de135d7bce3ffb2af2263
pro2200 <pro_2200@hotmail.com>
2023-12-15 10:41:50 -0500

    responsiveness and Translation

--- client/src/components/FilmMakerDashboard/Epks/EpkTextInfo.js
+++ client/src/components/FilmMakerDashboard/Epks/EpkTextInfo.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useTranslation } from 'react-i18next';
+
 
 export default function EpkTextInfo() {
+  const { t } = useTranslation();
     return (
         <div className="tw-flex tw-flex-col tw-justify-start tw-items-start ">
           <p className="tw-text-sm tw-font-bold tw-text-[#1E0039] tw-text-center">
-            Find all your film EPKs that you’ve created here.
+            {t("Find all your film EPKs that you’ve created here.")}
           </p>
         </div>
       );
--- client/src/components/FilmMakerDashboard/Notifications/NotificationTextInfo.js
+++ client/src/components/FilmMakerDashboard/Notifications/NotificationTextInfo.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { useTranslation } from 'react-i18next';
 
 export default function NotificationTextInfo() {
+  const { t } = useTranslation();
     return (
         <div className="tw-flex tw-flex-col tw-justify-start tw-items-start ">
           <p className="tw-text-sm tw-font-bold tw-text-[#1E0039] tw-text-center">
-          Find all the users film industry professionals who favourite and follow your film, and those who requests access to your protected EPKs.
+          {t("Find all the users film industry professionals who favourite and follow your film, and those who requests access to your protected EPKs.")}
           </p>
         </div>
       );
