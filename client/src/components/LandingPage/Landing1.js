efcf9045e98e5aa4f324671dc18ebc6a9c3fe1cb
Malek Kazal <ghazal.malek@gmail.com>
2024-06-20 10:29:00 -0400

    Create EPK btn landingPage, Thinner homePage white section

--- client/src/components/LandingPage/Landing1.js
+++ client/src/components/LandingPage/Landing1.js
@@ -1,10 +1,10 @@
-import React, { useRef, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import axios from "axios";
+import React, { useRef, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 // import http from "../../http-common.js";
-import heroImage from "../../images/LandingPage_Hero.png";
-import {useTranslation} from 'react-i18next';
+import heroImage from '../../images/LandingPage_Hero.png';
+import { useTranslation } from 'react-i18next';
 import { gsap } from 'gsap';
 
 const Landing1 = () => {
@@ -26,7 +26,7 @@ const Landing1 = () => {
       //dispatch({ type: "LOGIN", payload: data });
 
       console.log(data);
-      navigate("/uploadEpk");
+      navigate('/uploadEpk');
     } catch (error) {
       // console.log(error.response.message);
     }
@@ -42,7 +42,7 @@ const Landing1 = () => {
   // const firstElement = Math.max(0, lastElement - 4);
   // const filteredFepks = fepks.slice(firstElement, lastElement);
 
-  //GSAP animation 
+  //GSAP animation
   const headingRef = useRef(null);
   const subHeadingRef = useRef(null);
   const imageRef = useRef(null);
@@ -54,22 +54,30 @@ const Landing1 = () => {
 
     let tl;
     if (heading && subHeading && image) {
-      tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+      tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
 
       tl.from(heading, {
         opacity: 0,
         y: -50,
         duration: 1,
       })
-        .from(subHeading, {
-          opacity: 0,
-          y: -30,
-          duration: 1,
-        }, "-=0.5")
-        .from(image, {
-          opacity: 0,
-          duration: 1.5,
-        }, "-=0.5");
+        .from(
+          subHeading,
+          {
+            opacity: 0,
+            y: -30,
+            duration: 1,
+          },
+          '-=0.5'
+        )
+        .from(
+          image,
+          {
+            opacity: 0,
+            duration: 1.5,
+          },
+          '-=0.5'
+        );
     }
 
     return () => {
@@ -80,42 +88,45 @@ const Landing1 = () => {
 
   return (
     <>
-      <div className='tw-bg-midnight lg:tw-h-[100vh]'>
-        <div className='md:tw-p-24'>
-          <div className='tw-flex tw-w-full tw-items-center'>
-            <div className='tw-p-7 lg:tw-w-2/4'>
-              <h1 ref={headingRef} className='tw-mt-6 tw-text-start tw-text-6xl tw-font-bold tw-text-white md:tw-text-7xl'>
-                {t("Film marketing.")}
+      <div className="tw-bg-midnight lg:tw-h-[100vh]">
+        <div className="md:tw-p-24">
+          <div className="tw-flex tw-w-full tw-items-center">
+            <div className="tw-p-7 lg:tw-w-2/4">
+              <h1
+                ref={headingRef}
+                className="tw-mt-6 tw-text-start tw-text-6xl tw-font-bold tw-text-white md:tw-text-7xl"
+              >
+                {t('Film marketing.')}
                 <br />
-                <span ref={subHeadingRef}>{t("Refined.")}</span>
+                <span ref={subHeadingRef}>{t('Refined.')}</span>
               </h1>
-              <p className='tw-my-12 tw-text-xl tw-font-semibold tw-text-white md:tw-text-2xl'>
+              <p className="tw-my-12 tw-text-xl tw-font-semibold tw-text-white md:tw-text-2xl">
                 {t(
                   "Promote your film to industry professionals with KinoKlik's free"
                 )}
                 <br />
-                <span className='tw-text-[#FF00F5]'>
-                  {t("Electronic Press Kit")}
-                </span>{" "}
-                {t("software!")}
+                <span className="tw-text-[#FF00F5]">
+                  {t('Electronic Press Kit')}
+                </span>{' '}
+                {t('software!')}
               </p>
-              <div className='tw-mt-4 tw-grid-cols-2 sm:tw-mt-6'>
+              <div className="tw-mt-4 tw-grid-cols-2 sm:tw-mt-6">
                 <a
-                  className='tw-mr-4 tw-inline-block tw-rounded-lg tw-bg-[#FF00F5] tw-px-3 tw-py-1 tw-text-xl tw-font-bold tw-tracking-wider tw-text-midnight  tw-shadow-lg hover:tw--translate-y-0.5 hover:tw-bg-violet-600 focus:tw-outline-none sm:tw-text-base md:tw-px-4 md:tw-py-2'
-                  href='/uploadFepk'
+                  className="tw-mr-4 tw-inline-block tw-rounded-lg tw-bg-[#FF00F5] tw-px-3 tw-py-1 tw-text-xl tw-font-bold tw-tracking-wider tw-text-midnight  tw-shadow-lg hover:tw--translate-y-0.5 hover:tw-bg-violet-600 focus:tw-outline-none sm:tw-text-base md:tw-px-4 md:tw-py-2"
+                  href="/signup"
                 >
-                  {t("Create EPK")}
+                  {t('Create EPK')}
                 </a>
                 <a
-                  className='tw-inline-block tw-rounded-lg tw-bg-white tw-px-3 tw-py-1 tw-text-xl tw-font-bold tw-tracking-wider tw-text-[#1E0039] tw-shadow-lg hover:tw--translate-y-0.5 hover:tw-bg-violet-600 focus:tw-outline-none sm:tw-text-base md:tw-px-4 md:tw-py-2'
-                  href='/catalog'
+                  className="tw-inline-block tw-rounded-lg tw-bg-white tw-px-3 tw-py-1 tw-text-xl tw-font-bold tw-tracking-wider tw-text-[#1E0039] tw-shadow-lg hover:tw--translate-y-0.5 hover:tw-bg-violet-600 focus:tw-outline-none sm:tw-text-base md:tw-px-4 md:tw-py-2"
+                  href="/catalog"
                 >
-                  {t("Browse EPKs")}
+                  {t('Browse EPKs')}
                 </a>
               </div>
             </div>
 
-            <div className='tw-invisible tw-mx-auto tw-w-0 lg:tw-visible lg:tw-w-2/4'>
+            <div className="tw-invisible tw-mx-auto tw-w-0 lg:tw-visible lg:tw-w-2/4">
               {/* {filteredFepks.map((fepk, index) => (
                 <img
                   key={index}
@@ -126,7 +137,12 @@ const Landing1 = () => {
                   alt=''
                 />
               ))} */}
-              <img ref={imageRef} src={heroImage} alt='' className=' tw-h-full' />
+              <img
+                ref={imageRef}
+                src={heroImage}
+                alt=""
+                className=" tw-h-full"
+              />
             </div>
           </div>
         </div>
