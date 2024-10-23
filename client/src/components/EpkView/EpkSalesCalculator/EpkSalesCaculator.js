0cdf09172814d6dac7cbb1650ce23a5511d3bc3c
Israel Uzcategui <israel.uzcategui@outlook.com>
2024-10-23 11:44:18 -0400

    further fix for epk view calculator responsiveness
    added padding to symbols and put labels tags to prevent text wrapping

--- client/src/components/EpkView/EpkSalesCalculator/EpkSalesCaculator.js
+++ client/src/components/EpkView/EpkSalesCalculator/EpkSalesCaculator.js
@@ -18,23 +18,28 @@ const EpkSalesCalculator = () => {
 
     return (
         <div className='tw-bg-white tw-items-center tw-justify-center tw-border-2 tw-border-gray-400 tw-w-[100%] tw-p-4 tw-mx-auto  tw-mt-10 tw-mb-10'>
-            <div className="calculator-container" style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                padding: "20px",
-                flexWrap: "wrap" // Allows wrapping on smaller screens
-            }}>
-                <div className="input-field" style={{ minWidth: "150px", flex: 1 }}>
-                    Total Audience Reach:
+            <div
+                className="calculator-container"
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    padding: '20px',
+                    flexWrap: 'wrap',
+                    gap: '15px',
+                    alignItems: 'center',
+                }}
+            >
+                <div className="input-field" style={{ flex: 1, minWidth: '150px' }}>
+                    <label style={{ whiteSpace: 'nowrap' }}>Total Audience Reach:</label>
                     <input
                         style={{
-                            height: "30px",
-                            width: "100%",
-                            borderRadius: "5px",
-                            marginBottom: "20px",
-                            boxShadow: "1px 2px 9px #311465",
-                            textAlign: "left",
+                            height: '30px',
+                            width: '100%',
+                            borderRadius: '5px',
+                            marginBottom: '20px',
+                            boxShadow: '1px 2px 9px #311465',
+                            textAlign: 'left',
                         }}
                         type="number"
                         value={totalAudienceReach}
@@ -42,18 +47,18 @@ const EpkSalesCalculator = () => {
                     />
                 </div>
 
-                <div> <br /> <span style={{ fontWeight: "bold" }}> X </span> </div>
+                <div style={{ fontWeight: 'bold', padding: '0 10px' }}>X</div>
 
-                <div className="input-field" style={{ minWidth: "150px", flex: 1 }}>
-                    Film Price ($):
+                <div className="input-field" style={{ flex: 1, minWidth: '150px' }}>
+                    <label style={{ whiteSpace: 'nowrap' }}>Film Price ($):</label>
                     <input
                         style={{
-                            height: "30px",
-                            width: "100%",
-                            borderRadius: "5px",
-                            marginBottom: "20px",
-                            boxShadow: "1px 2px 9px #311465",
-                            textAlign: "left",
+                            height: '30px',
+                            width: '100%',
+                            borderRadius: '5px',
+                            marginBottom: '20px',
+                            boxShadow: '1px 2px 9px #311465',
+                            textAlign: 'left',
                         }}
                         type="number"
                         value={filmPrice}
@@ -61,18 +66,18 @@ const EpkSalesCalculator = () => {
                     />
                 </div>
 
-                <div> <br /> <span style={{ fontWeight: "bold" }}> X </span> </div>
+                <div style={{ fontWeight: 'bold', padding: '0 10px' }}>X</div>
 
-                <div className="input-field" style={{ minWidth: "150px", flex: 1 }}>
-                    Engagement Rate (%):
+                <div className="input-field" style={{ flex: 1, minWidth: '150px' }}>
+                    <label style={{ whiteSpace: 'nowrap' }}>Engagement Rate (%):</label>
                     <input
                         style={{
-                            height: "30px",
-                            width: "100%",
-                            borderRadius: "5px",
-                            marginBottom: "20px",
-                            boxShadow: "1px 2px 9px #311465",
-                            textAlign: "left",
+                            height: '30px',
+                            width: '100%',
+                            borderRadius: '5px',
+                            marginBottom: '20px',
+                            boxShadow: '1px 2px 9px #311465',
+                            textAlign: 'left',
                         }}
                         type="number"
                         value={engagementRate}
@@ -80,17 +85,17 @@ const EpkSalesCalculator = () => {
                     />
                 </div>
 
-                <div> <br /> <span style={{ fontWeight: "bold" }}> -> </span> </div>
+                <div style={{ fontWeight: 'bold', padding: '0 10px' }}>-></div>
 
                 <Button
                     style={{
-                        width: "120px",
-                        boxShadow: "1px 2px 9px #311465",
-                        backgroundColor: "#ffffff",
-                        color: "#000",
-                        fontWeight: "bold",
-                        marginTop: "20px",
-                        minWidth: "150px",
+                        width: '120px',
+                        boxShadow: '1px 2px 9px #311465',
+                        backgroundColor: '#ffffff',
+                        fontWeight: 'bold',
+                        marginTop: '20px',
+                        minWidth: '150px',
+                        color: '#311465',
                     }}
                     type="primary"
                     onClick={handleCalculate}
@@ -98,15 +103,15 @@ const EpkSalesCalculator = () => {
                     {t("Calculate")}
                 </Button>
 
-                <div> <br /> <span style={{ fontWeight: "bold" }}> = </span> </div>
+                <div style={{ fontWeight: 'bold', padding: '0 10px' }}>=</div>
 
                 <p
                     style={{
-                        marginTop: "20px",
-                        fontSize: "1em",
-                        fontWeight: "bold",
-                        minWidth: "150px",
-                        flex: 1
+                        marginTop: '20px',
+                        fontSize: '1em',
+                        fontWeight: 'bold',
+                        minWidth: '150px',
+                        flex: 1,
                     }}
                 >
                     Potential Sales: {potentialSales} $
